4388be264f91a6e3d9309d7f54e19dc86ec4194a
ialoysious <isaacaloysious@gmail.com>
2023-02-08 14:09:53 -0800

    wireframe draft

---
+++ app.js
@@ -0,0 +1,38 @@
+$(() => {
+	// console.log('hi');
+	// const $openBtn = $('#openModal');
+	// const $modal = $('#modal');
+	// const $closeBtn = $('#close');
+
+	// //Event Handlers
+	// const openModal = () => {
+	// 	$modal.css('display', 'block');
+	// };
+
+	// const closeModal = () => {
+	// 	$modal.css('display', 'none');
+	// };
+
+	// //Event Listeners
+	// $openBtn.on('click', openModal);
+	// $closeBtn.on('click', closeModal);
+	const modal = document.getElementById('id01');
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = 'none';
+		}
+	};
+	document.getElementById('resumeLink').style.width = '32.9%';
+	document.getElementById('gitHubLink').style.width = '32.9%';
+	document.getElementById('contact').style.width = '32.9%';
+
+	const resumeLink = document.getElementById('resumeLink');
+	const resumeLoad = document.getElementById('resumeLoad');
+	resumeLink.addEventListener('click', function () {
+		if (resumeLoad.style.display === 'block') {
+			resumeLoad.style.display = 'none';
+		} else {
+			resumeLoad.style.display = 'block';
+		}
+	});
+});
